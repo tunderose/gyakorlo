af4666ac42d7254aab238bfd968f606966b736a8
tunderose <pe_tu@ymail.com>
2024-04-17 01:14:48 +0200

    first commit

---
+++ exercise_12_04_24_1_2.py
@@ -0,0 +1,24 @@
+print("Twinkle, twinkle, little star, \n \t \t How I wonder what you are!")
+print("\t \t \t Up above the world so high,\n \t \t \t Like a diamond in the sky.")
+print("Twinkle, twinkle, little star, \n \t How I wonder what you are.")
+
+line_1 = 'Twinkle, twinkle, little star,'
+line_2 = '\n \t \tHow I wonder what you are'
+line_3 = '\t \tUp above the world so high,\n \t \t \t'
+line_4 = 'Like a diamond in the sky.'
+
+print(str(line_1 + line_2 + "."))
+print(str(line_3 + line_4))
+print(str(line_1 + line_2 + "!"))
+
+# even and odd numbers
+
+number = int(input("Please enter an (integer) number: "))
+if not 0 != number % 2:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+
+
+
+
